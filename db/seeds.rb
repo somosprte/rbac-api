62a32cafb7ff1b99a8c7a36a415392fafdfc5484
Flavio Junior <flpgjr@gmail.com>
2019-04-24 09:36:11 -0300

    Seed create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+person = User::Person.find_by(email:"flavio@prte.com.br")
+if person.nil?
+    person = User::Person.create(name:"Flavio", email:"flavio@prte.com.br", birthday:"1989-10-21")
+end
+
+auth = Auth::User.find_by(username:"flavio@prte.com.br")
+if auth.nil?
+    Auth::User.create(username:"flavio@prte.com.br", password:'123456', usereable:person)
+end
+
+person_2 = User::Person.find_by(email:"nivaldo@prte.com.br")
+if person_2.nil?
+    person_2 = User::Person.create(name:"Nivaldo", email:"nivaldo@prte.com.br", birthday:"1989-11-21")
+end
+
+auth_2 = Auth::User.find_by(username:"nivaldo@prte.com.br")
+if auth_2.nil?
+    Auth::User.create(username:"nivaldo@prte.com.br", password:'123456', usereable:person_2)
+end
