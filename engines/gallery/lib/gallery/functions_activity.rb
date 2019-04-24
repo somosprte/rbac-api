885da4dddc7bb7440e006873afda9c666e52be9a
Flavio Junior <flpgjr@gmail.com>
2019-04-24 19:16:49 -0300

    Lib sync functions activity

--- engines/gallery/lib/gallery/functions_activity.rb
+++ engines/gallery/lib/gallery/functions_activity.rb
@@ -1,23 +1,25 @@
-# module Gallery
-#     module FunctionsActivity
-#         def self.crud_general_materials(activity, activity_params, update=nil)
-#             ids = []
-#             activity_params[:general_materials].each do |material|
-#                 material = Gallery::GeneralMaterial.find_by(material[:id])
-#                 if material
-#                     unless activity.activity_materials.find_by(material:material)
-#                         activity.activity_materials.create(material:material, quantity:material[:quantity])
-#                     end
-#                 end
-#                 ids.push(material[:id])
-#             end
-#             if update == true
-#                     sync_materials = Gallery::ActivityMaterial.where(activity_id:activity.id).where.not(material_id:ids)
-#                     sync_materials.each do |sync|
-#                     sync.destroy
-#                 end
-#             end
-#             return activity
-#         end
-#     end
-# end
+module Gallery
+    module FunctionsActivity
+        def self.crud_general_materials(activity, activity_params, update=nil)
+            ids = []
+            activity_params[:general_materials].each do |material|
+                material = Gallery::GeneralMaterial.find_by(material[:id])
+                if material
+                    unless activity.activity_general_materials.find_by(material:material)
+                        activity.activity_general_materials.create(material:material, quantity:material[:quantity])
+                    else
+                        activity.activity_general_materials.update(quantity:material[:quantity])
+                    end
+                end
+                ids.push(material[:id])
+            end
+            if update == true
+                    sync_materials = Gallery::ActivityGeneralMaterial.where(activity_id:activity.id).where.not(material_id:ids)
+                    sync_materials.each do |sync|
+                    sync.destroy
+                end
+            end
+            return activity
+        end
+    end
+end
