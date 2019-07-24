c503514f5f65c8982adaf19ef02482433861311e
Osvaldo Aurélio <osvaldo.aurelio@outlook.com>
2019-07-24 11:40:47 -0300

    fixing when there is not exist general_material or inspiration fields

--- engines/gallery/lib/gallery/functions_activity.rb
+++ engines/gallery/lib/gallery/functions_activity.rb
@@ -4,20 +4,22 @@ module Gallery
   module FunctionsActivity
     def self.crud_general_materials(activity, activity_params, update = nil)
       ids = []
-      activity_params[:activity][:general_materials].each do |material|
-        material_general = Gallery::GeneralMaterial.find_by(id: material[:id])
-        if material_general
-          if activity.activity_general_materials.find_by(general_material_id: material[:id])
-            activity.activity_general_materials.update(quantity: material[:quantity])
-          else
-            activity.activity_general_materials.create(general_material_id: material[:id], quantity: material[:quantity])
-            end
+      if activity_params[:activity].key?(:general_materials) 
+        activity_params[:activity][:general_materials].each do |material|
+          material_general = Gallery::GeneralMaterial.find_by(id: material[:id])
+          if material_general
+            if activity.activity_general_materials.find_by(general_material_id: material[:id])
+              activity.activity_general_materials.update(quantity: material[:quantity])
+            else
+              activity.activity_general_materials.create(general_material_id: material[:id], quantity: material[:quantity])
+              end
+          end
+          ids.push(material[:id])
         end
-        ids.push(material[:id])
-      end
-      if update == true
-        sync_materials = Gallery::ActivityGeneralMaterial.where(activity_id: activity.id).where.not(general_material_id: ids)
-        sync_materials.each(&:destroy)
+        if update == true
+          sync_materials = Gallery::ActivityGeneralMaterial.where(activity_id: activity.id).where.not(general_material_id: ids)
+          sync_materials.each(&:destroy)
+        end
       end
       activity
     end
@@ -44,18 +46,20 @@ module Gallery
 
     def self.crud_inspirations(activity, activity_params, update = nil)
       ids = []
-      activity_params[:activity][:inspirations_ids].each do |inspiration|
-        gallery_inspiration = Gallery::Activity.find_by(id: inspiration)
-        if gallery_inspiration
-          unless activity.inspirations.find_by(activity_two_id: inspiration)
-            activity.inspirations.create(activity_two_id: inspiration, title: gallery_inspiration.title)
+      if activity_params[:activity].key?(:inspirations_ids)
+        activity_params[:activity][:inspirations_ids].each do |inspiration|
+          gallery_inspiration = Gallery::Activity.find_by(id: inspiration)
+          if gallery_inspiration
+            unless activity.inspirations.find_by(activity_two_id: inspiration)
+              activity.inspirations.create(activity_two_id: inspiration, title: gallery_inspiration.title)
+            end
           end
+          ids.push(inspiration)
         end
-        ids.push(inspiration)
-      end
-      if update == true
-        sync_inspirations = Gallery::Inspiration.where(activity_id: activity.id).where.not(activity_two_id: ids)
-        sync_inspirations.each(&:destroy)
+        if update == true
+          sync_inspirations = Gallery::Inspiration.where(activity_id: activity.id).where.not(activity_two_id: ids)
+          sync_inspirations.each(&:destroy)
+        end
       end
       activity
     end
